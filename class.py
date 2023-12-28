0b0b2f1f1b9e771754a31e754f696b358491bd25
Akpevwe <essiakpevwepraise@gmail.com>
2023-12-28 17:29:21 +0100

    inital commit

---
+++ class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+class decorator_class(object):
+    def __init__(self, original_function):
+        self.original_function = original_function
+
+    def __call__(self, *args, **kwargs):
+        print('wrapper executed this before {}'.format(self.original_function.__name__))
+        return self.original_function(*args, **kwargs)
+
+
+@decorator_class
+def display():
+    print('display function ran')
+
+
+@decorator_class
+def display_info(name, age):
+    print('display_into ran with arguments ({}, {})'.format(name, age))
+
+
+display()
---
+++ decorators.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def decorator_function(original_function):
+    def wrapper_function(*args, **kwargs):
+        print('wrapper executed this before {}'.format(original_function.__name__))
+        return original_function(*args, **kwargs)
+    return wrapper_function
+
+
+
+@decorator_function
+def display():
+    print('display function ran')
+
+@decorator_function
+def display_info(name, age):
+    print('display_info ran with arguments ({}, {})'.format(name, age))
+
+display_info('John', 25)
+
+#display()
+
+#decorator_display = decorator_function(display)
+#decorator_display()
