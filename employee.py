059572d811aba54ccb698300530d8b9c3e576d04
Akpevwe <essiakpevwepraise@gmail.com>
2023-12-28 23:19:46 +0100

    decorator

--- employee.py
+++ employee.py
@@ -14,13 +14,29 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
+
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
+
 emp_1 = Employee('John', 'Smith')
+
+emp_1.fullname = 'Corey Schafer'
 
 emp_1.first = 'Jim'
 
 print(emp_1.first)
 print(emp_1.email)
 print(emp_1.fullname)
+
+del emp_1.fullname
 #print(emp_1.first)
 #print(emp_1.email())
 #print(emp_1.fullname())
