b715a3b7d8f264803d1acfad2e053391d50a42b0
Akpevwe <essiakpevwepraise@gmail.com>
2025-04-20 20:07:55 +0100

    question

---
+++ Binary-Tree/search_2d_matrices.py
@@ -0,0 +1,23 @@
+"""
+link: https://leetcode.com/problems/search-a-2d-matrix/?envType=problem-list-v2&envId=binary-search
+
+Problem: Search a 2D Matrix
+Given an m x n matrix, return true if target is in the matrix and false otherwise.
+You must write a solution in O(log(m * n)) time complexity.
+Example 1:
+
+Input: matrix = [[1,3,5],[7,9,11],[12,13,15]], target = 9
+
+Output: true
+
+Example 2:
+
+
+"""
+from typing import List
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        return False
