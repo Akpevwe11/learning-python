0bc4cd989465838bfce541d75bd11df5c0ef927f
Akpevwe <essiakpevwepraise@gmail.com>
2025-04-25 19:51:06 +0100

    find minimum in a rotated sorted array

---
+++ Binary-Tree/problem_153.py
@@ -0,0 +1,20 @@
+"""
+link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array?envType=problem-list-v2&envId=binary-search
+
+Problem: Find Minimum in Rotated Sorted Array
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times.
+For example, the array nums = [0,1,2,4,5,6,7] might become:
+[4,5,6,7,0,1,2] after some rotations.
+Find the minimum element of this rotated sorted array.
+You must write an algorithm that runs in O(log n) time complexity.
+Example 1:
+Input: nums = [3,4,5,1,2]
+Output: 1
+Example 2:
+Input: nums = [4,5,6,7,0,1,2]
+Output: 0
+Example 3:
+Input: nums = [1]
+Output: 1
+
+"""
