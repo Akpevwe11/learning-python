e971d1209fe5e30d22818925ea23699dc53894e4
Akpevwe <essiakpevwepraise@gmail.com>
2025-04-27 21:33:59 +0100

    finding peak element

---
+++ Binary-search/leetcode_162.py
@@ -0,0 +1,26 @@
+"""
+ link: https://leetcode.com/problems/find-peak-element?envType=problem-list-v2&envId=binary-search
+
+Problem: Find Peak Element
+A peak element in an array is an element that is strictly greater than its neighbors.
+Given an integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+You may imagine that nums[-1] = nums[n] = -∞.
+In other words, an element is a peak if it is greater than its neighbors.
+You must write an algorithm that runs in O(log n) time complexity.
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+
+Example 2:
+Input: nums = [1,2,1,3,5,6,4]
+Output: 1 or 5
+Explanation: Your solution should be in logarithmic complexity.
+Example 3:
+Input: nums = [1]
+Output: 0
+
+Example 4:
+Input: nums = [1,2]
+Output: 1
+"""
