6ec189a3f6123ce2231ba32e7f943320b4d6ce59
Akpevwe <essiakpevwepraise@gmail.com>
2025-05-04 00:49:00 +0100

    implementation in linear time

--- Sliding-window/leetcode-209.py
+++ Sliding-window/leetcode-209.py
@@ -20,3 +20,20 @@ Output: 1
 
 
 from typing import List
+
+
+def minSubArrayLen(target: int, nums: List[int]):
+    left = 0
+    total = 0
+    min_len = float('inf')
+
+    for right in range(len(nums)):
+        total += nums[right]
+
+        while total >=target:
+            min_len = min(min_len, right - left + 1)
+            total-= nums[left]
+            left += 1
+
+    return 0 if min_len == float('inf') else min_len
+
