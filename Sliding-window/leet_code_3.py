55d33c77f253b3ef99d0f7a7487ee07676fee825
Akpevwe <essiakpevwepraise@gmail.com>
2025-05-06 22:44:09 +0100

    length of longest substring

--- Sliding-window/leet_code_3.py
+++ Sliding-window/leet_code_3.py
@@ -28,3 +28,22 @@ Notice that the answer must be a substring, "pwke" is a subsequence and not a su
 
 """
 
+
+
+def length_of_longest_substring(s: str) -> int:
+    char_set = set()
+    left = 0
+    max_length = 0
+
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+        char_set.add(s[right])
+        max_length = max(max_length, left - right + 1)
+
+    return max_length
+
+# Example usage
+s = "abcabcbb"
+print(length_of_longest_substring(s))  # Output: 3
