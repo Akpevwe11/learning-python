96e3ccef4a8021bf2af13bae1435cbdb13b71548
Akpevwe <essiakpevwepraise@gmail.com>
2025-05-10 03:29:44 +0100

    contains near duplicate

--- Sliding-window/leetcode_219.py
+++ Sliding-window/leetcode_219.py
@@ -20,3 +20,12 @@ Input: nums = [1,2,3,1,2,3], k = 2
 Output: false
 
 """
+
+def containsNearbyDuplicate(nums, k):
+    num_indices = {}
+
+    for i, num in enumerate(nums):
+        if num in num_indices and i - num_indices[num] <= k:
+            return True
+        num_indices[num] = i
+    return False
